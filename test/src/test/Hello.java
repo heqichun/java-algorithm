6cdf3fa6d703b6c74f9d69544ea6888db0e598ef
heqichun <747204379@qq.com>
2017-11-07 19:00:29 +0800

    changes log

--- test/src/test/Hello.java
+++ test/src/test/Hello.java
@@ -3,6 +3,9 @@ package test;
 public class Hello {
 
 	public static void main(String[] args) {
+		
+		String str = "http://zcm.bulidnet.cn/wxpr/#/subscription-list/project/true";
+		System.out.println(str.substring(str.indexOf("/wxpr/#/")+8));
 	}
 
 }
